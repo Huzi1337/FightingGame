d0d2df0d80cf4692444be1909116c703c876a93b
Huzi1337 <krzysztof.jagoda99@gmail.com>
2023-08-22 12:04:54 +0200

    Muted the sound on initial site render.

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ c?.fillRect(0, 0, canvas.width, canvas.height);
 const background = new Sprite({ x: 0, y: 0 }, ["/bg.png"]);
 
 const mainTheme = new Audio("/music/theme1.mp3");
-mainTheme.muted = false;
+mainTheme.muted = true;
 mainTheme.play();
 mainTheme.loop = true;
 
